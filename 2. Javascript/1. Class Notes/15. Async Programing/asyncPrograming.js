92ae6fe13a8367bcd7f29a60f73157e18beecceb
MnrGDKL <munirgedikli.2021@gmail.com>
2022-03-10 18:06:36 +0300

    commited

--- 2. Javascript/1. Class Notes/15. Async Programing/asyncPrograming.js
+++ 2. Javascript/1. Class Notes/15. Async Programing/asyncPrograming.js
@@ -85,9 +85,120 @@ console.log(id2);
 //         console.log("TIME IS OVER");}
 // }, 1000);
 
+function wait(ms) {
+  const start = new Date().getTime();
+  let end = start;
+  while (end < start + ms) {
+    end = new Date().getTime();
+  }
+}
 
 // ***********************
 // *    Promise()
 // ***********************
 
-new Promise()
+
+//? Eaxmple-1
+
+// let data = Math.floor(Math.random()*2);
+
+// const promise = new Promise((resolve,reject)=>{
+//   const succ = 5
+//   const err = new Error("Failure!");
+//   data == true ? resolve(succ) : reject(err);
+// })
+
+// promise
+//   .then((positive_result)=>{
+//     console.log(positive_result);
+//     return positive_result*positive_result;
+//   })
+//   .then((sec_pos_res)=>{
+//     console.log(sec_pos_res);
+//   })
+//   .catch((negative_result)=>{
+//     console.log(negative_result);
+//   })
+//   .then(()=>{
+//     console.log("data =",data);
+//   })
+
+//?Example-2
+//? Çay Demleme
+
+const cayDemle = ()=>{
+  suyuKaynat()
+  .then((durum1) => {
+    console.log(durum1); //! durum1 = "Su Kaynadı"
+    return cayEkle();  
+  })
+  .then((durum2)=>{
+    console.log(durum2); //! durum2 = cayEkle() => "Çay Eklendi"
+    bekle(1500);
+    return afiyet();
+  })
+  .then((durum3)=>{
+    console.log(durum3); //! durum3 = afiyet() => "Çayınız Demlendi, Afiyet Olsun!"
+  })
+  .catch((err) => {
+    console.log(err); //! err = reject() => "Kettle Arızalı" || "Çay Bitmiş"
+  });
+}
+
+const suyuKaynat = ()=>{
+  return new Promise((resolve,reject)=>{
+    const nasip = Math.floor(Math.random() * 3);
+    if (nasip){
+      bekle(1000);
+      resolve("Su Kaynadı");
+    }
+    reject(new Error("Kettle Arızalı"));
+  })
+}
+
+const cayEkle = ()=>{
+  return new Promise((resolve,reject)=>{
+    const cayNasibi = Math.floor(Math.random() * 3);
+    if (cayNasibi) {
+      bekle(500);
+      resolve("Çay Eklendi");
+    }
+    reject("Çay Bitmiş!");
+  });
+}
+
+const bekle = (ms)=>{
+    const start = new Date().getTime();
+    let end = start;
+    while (end < start + ms) {
+      end = new Date().getTime();
+    }
+  }
+
+const afiyet = ()=>{
+  return "Çayınız Demlendi, Afiyet Olsun!";
+}
+
+
+// cayDemle();
+
+// ***********************
+// *    Async() Await
+// ***********************
+
+
+const cayDemle2 = async () =>{
+  try {
+    const durum1 = await suyuKaynat(); console.log(durum1);   //! durum1 = resolve() => "Su Kaynadı"
+    await bekle(1500);
+    const durum2 = await cayEkle(); console.log(durum2);   //! durum2 = resolve() => "Çay Eklendi"
+    await bekle(1500);
+    const durum3 = await afiyet(); console.log(durum3);   //! durum3 = "Çayınız Demlendi, Afiyet Olsun!"
+  } catch (error) { 
+    console.error(error);   //! error = reject() => "Kettle Arızalı"
+                            //! error = reject() =>  "Çay Bitmiş"
+  }
+}
+
+cayDemle2();
+
---
+++ 2. Javascript/3. Projects/JS-Project-007--Movie-Seat-App/script.js
@@ -0,0 +1,92 @@
+const seatSelection   = document.querySelector(".container");
+const movies          = document.querySelector("#movie");
+const clearData       = document.querySelector(".clear");
+
+const count           = document.querySelector("#count");
+const film            = document.querySelector("#film");
+const total           = document.querySelector("#total");
+
+
+function setLocal(){
+      localStorage.setItem("Selected Seats", selectedSeats());
+      localStorage.setItem("Selected Movie", selectedMovie())
+      localStorage.setItem("Selected Movie Price",selectedMoviePrice());
+}
+
+function selectedSeats(){
+      const seats = document.querySelectorAll(".row .seat");
+      let selectedSeats = [];
+      seats.forEach((seat, index)=>{
+            if (seat.classList.contains("selected")){
+                  selectedSeats.push(index);
+            }
+      })
+      return selectedSeats;
+}
+
+function selectedMovie(){
+      return movies.options[movies.selectedIndex].innerText.split("(")[0];
+}
+
+function selectedMoviePrice(){
+      return movies.options[movies.selectedIndex].value;
+}
+
+function calculations(){
+      count.textContent = localStorage.getItem("Selected Seats").split(",").length;
+      film.textContent  = localStorage.getItem("Selected Movie");
+      total.textContent = localStorage.getItem("Selected Movie Price") * localStorage.getItem("Selected Seats").split(",").length;
+}
+
+movies.addEventListener("change",()=>{
+     localStorage.setItem("Selected Movie", selectedMovie())
+     localStorage.setItem("Selected Movie Price",selectedMoviePrice());
+     calculations();
+});
+
+
+seatSelection.addEventListener("click", (e)=>{
+     if (e.target.classList.contains("seat") 
+            && !e.target.classList.contains("occupied")){
+                  e.target.classList.toggle("selected");
+                  document.querySelector(".screen").innerText = "Data Stored";
+                  setTimeout(() => {
+                        document.querySelector(".screen").innerText = "";
+                  }, 500);
+            }
+      console.log(selectedSeats());
+      setLocal();
+      calculations();
+
+
+});
+
+window.addEventListener("load", ()=>{
+
+      const seats = document.querySelectorAll(".row .seat");
+      const selectedSeats = localStorage.getItem("Selected Seats").split(",");
+      for (const seat of selectedSeats) {
+            seats[seat].classList.add("selected");
+      }
+      const options = document.querySelectorAll("#movie option")
+      options.forEach((movie, index) => {
+            if (movie.value == localStorage.getItem("Selected Movie Price")){
+                  movie.setAttribute("selected",true);
+            }
+      });
+      calculations();
+});
+
+clearData.addEventListener("click", ()=>{
+      localStorage.removeItem("Selected Seats");
+      localStorage.removeItem("Selected Movie");
+      localStorage.removeItem("Selected Movie Price");
+      document.querySelector(".screen").innerText = "Data Removed";
+      setTimeout(() => {
+            window.location.reload();
+      }, 500);
+      setTimeout(() => {
+            document.querySelector(".screen").innerText = "";
+      }, 500);
+})
+
