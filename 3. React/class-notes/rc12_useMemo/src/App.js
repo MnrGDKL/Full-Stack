a48f649dd94f3a5d6b56286a4364f58c68a5896e
MnrGDKL <munirgedikli.2021@gmail.com>
2022-04-16 12:04:03 +0300

    b

---
+++ 3. React/class-notes/rc12_useMemo/src/App.js
@@ -0,0 +1,11 @@
+import UseRefComponent from "./components/UseRefComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <UseRefComponent />
+    </div>
+  );
+}
+
+export default App;
