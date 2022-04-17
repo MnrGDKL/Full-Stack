478439d5c6f999b4ea389aa75d488c1be7df1291
MnrGDKL <munirgedikli.2021@gmail.com>
2022-04-17 11:31:17 +0300

    aa

--- 3. React/playground/my-app/src/App.js
+++ 3. React/playground/my-app/src/App.js
@@ -1,21 +1,11 @@
 import React from 'react'
 import "./App.css";
+import Floors from './components/Floors';
 
 const App = () => {
   return (
     <div>
-      <div>
-      <div className='floors'>5</div>
-      <button >UP</button>
-      <button>DOWN</button>
-
-      </div>
-      <div className='floors'>4</div>
-      <div className='floors'>3</div>
-      <div className='floors'>2</div>
-      <div className='floors'>1</div>
-      <div className='floors'>0</div>
-
+      <Floors/>
     </div>
   )
 }
