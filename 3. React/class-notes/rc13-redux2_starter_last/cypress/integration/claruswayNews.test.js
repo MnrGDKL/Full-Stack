693401d97efb853c3dae226c91e01b2fcc1c565f
MnrGDKL <munirgedikli.2021@gmail.com>
2022-04-21 00:29:53 +0300

    aa

--- 3. React/class-notes/rc13-redux2_starter_last/cypress/integration/claruswayNews.test.js
+++ 3. React/class-notes/rc13-redux2_starter_last/cypress/integration/claruswayNews.test.js
@@ -1,12 +1,20 @@
 describe('Header test', () => {
   it('Clarusway events test', () => {
     console.log("Clarusway events test");
-    cy.visit('http://localhost:3000').wait(2000)
-    cy.contains("CLARUSWAY NEWS").click()
+    cy.visit('/').wait(2000)
+    cy.contains("CLARUSWAY NEWS").click().wait(2000)
     cy.url().should('include', '/login')
-    cy.contains("Register").click()
+  })
+  it('login email test', () => {
+    cy.visit("/").wait(2000)
+    cy.get("#email").type("Test")
+    cy.should('have.value', 'Text')
+    cy.pause()
+  })
+  it("Register control" , () => {
+    cy.contains("Register").click().wait(2000)
     cy.url().should('include', '/register')
+    cy.pause()
 
-    cy.contains("CLAUSEWAY NEWS").click()
   })
 })
