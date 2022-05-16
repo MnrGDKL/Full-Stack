a2eca11de736ef212fdb20a52d38094ee03ef8c1
MnrGDKL <munirgedikli.2021@gmail.com>
2022-05-16 22:19:07 +0300

    11

--- 4. Django/class-notes/004_Models-2/fscohort/models.py
+++ 4. Django/class-notes/004_Models-2/fscohort/models.py
@@ -6,6 +6,20 @@ class Student(models.Model):
   first_name = models.CharField(max_length=50)
   last_name = models.CharField(max_length=50)
   number = models.IntegerField()
+  about = models.TextField(null=True, blank=True)
+  avatar = models.ImageField(null=True, blank=True, upload_to="media/")
+  register_date = models.DateTimeField(auto_now_add=True)
+  update_date = models.DateTimeField(auto_now=True)
 
 
+  def __str__(self):
+    return f"{self.first_name.title()} {self.last_name.upper()}"
+
+  class Meta():
+    ordering = ["number"]
+    verbose_name_plural = "Ogrenciler"
+    # db_table = "students_Table"
   
+
+
+
