1845c47e457bdc0bcf59a7266a0224d3d3683167
MnrGDKL <munirgedikli.2021@gmail.com>
2022-05-19 22:02:45 +0300

    111

---
+++ 4. Django/class-notes/008_Forms/student/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+
+from .forms import StudentForm
+
+def index(request):
+    return render(request, 'student/index.html')
+
+def student_page(request):
+    # print(request.POST)
+    form = StudentForm(request.POST or None)
+    if form.is_valid():
+        # print(form.cleaned_data.get("first_name"), form.cleaned_data.get("last_name"), form.cleaned_data.get("number"))
+        student = form.save()
+        if "profile_pic" in request.FILES:
+            student.profile_pic = request.FILES["profile_pic"]
+            student.save()
+        return redirect('student')
+    context={
+        'form': form
+    }
+
+    return render(request,'student/student.html', context)
