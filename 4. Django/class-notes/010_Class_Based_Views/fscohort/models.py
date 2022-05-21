599b58ea2d5f7cd60a4d50095f5a01003f2bb7c9
MnrGDKL <munirgedikli.2021@gmail.com>
2022-05-21 11:37:20 +0300

    33

--- 4. Django/class-notes/010_Class_Based_Views/fscohort/models.py
+++ 4. Django/class-notes/010_Class_Based_Views/fscohort/models.py
@@ -1,22 +1,31 @@
 from django.db import models
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    number = models.IntegerField(blank=True, null=True)
 
-    email = models.EmailField(max_length=154, unique=True, blank=True, null=True)
-    phone = models.CharField(max_length=50, unique=True, blank=True, null=True)
-
-    GENDER =(
-        ("1", "Female"),
-        ("2", "Male"),
-        ("3", "Other"),
-        ("4", "Prefer Not Say"),
-    )
-
-    gender = models.CharField(max_length=50, choices=GENDER)
-    number = models.IntegerField(unique=True, blank=True, null=True)
-    image = models.ImageField(upload_to="student/", default="avatar.png")
 
     def __str__(self):
-        return f"{self.number} {self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name}"
+
+# class Student(models.Model):
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+
+#     email = models.EmailField(max_length=154, unique=True, blank=True, null=True)
+#     phone = models.CharField(max_length=50, unique=True, blank=True, null=True)
+
+#     GENDER =(
+#         ("1", "Female"),
+#         ("2", "Male"),
+#         ("3", "Other"),
+#         ("4", "Prefer Not Say"),
+#     )
+
+#     gender = models.CharField(max_length=50, choices=GENDER)
+#     number = models.IntegerField(unique=True, blank=True, null=True)
+#     image = models.ImageField(upload_to="student/", default="avatar.png")
+
+#     def __str__(self):
+#         return f"{self.number} {self.first_name} {self.last_name}"
