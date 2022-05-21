30f89dd17efb54ec429352a30bfefd80fab76cf4
MnrGDKL <munirgedikli.2021@gmail.com>
2022-05-21 11:10:52 +0300

    3232

---
+++ 4. Django/class-notes/010_Class_Based_Views/fscohort/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+# from .views import home,student_list, student_add, student_detail, student_update,student_delete
+from .views import index
+
+urlpatterns = [
+    path('', index, name='home'),
+]
+
+# urlpatterns = [
+#     path('', home, name="home"),
+#     path('student_list/', student_list, name="list"),
+#     path('student_add/', student_add, name="add"),
+#     path('detail/<int:id>/', student_detail, name="detail"),
+#     path('update/<int:id>/', student_update, name="update"),
+#     path('delete/<int:id>/', student_delete, name="delete"),
+# ]
