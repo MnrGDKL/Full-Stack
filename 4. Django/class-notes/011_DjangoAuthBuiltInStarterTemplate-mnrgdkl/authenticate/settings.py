1ecd93212460d824c8943bc41281fa79b2846de5
MnrGDKL <munirgedikli.2021@gmail.com>
2022-05-26 23:03:13 +0300

    cc

--- 4. Django/class-notes/011_DjangoAuthBuiltInStarterTemplate-mnrgdkl/authenticate/settings.py
+++ 4. Django/class-notes/011_DjangoAuthBuiltInStarterTemplate-mnrgdkl/authenticate/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- 4. Django/class-notes/011_DjangoAuthBuiltInStarterTemplate-mnrgdkl/user_example/views.py
+++ 4. Django/class-notes/011_DjangoAuthBuiltInStarterTemplate-mnrgdkl/user_example/views.py
@@ -2,11 +2,13 @@ from multiprocessing import context
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 
 def home_view(request):    
     return render(request, "user_example/home.html")
 
+@login_required(login_url='/accounts/login/')
 
 def special(request):    
     return render(request, "user_example/special.html")
