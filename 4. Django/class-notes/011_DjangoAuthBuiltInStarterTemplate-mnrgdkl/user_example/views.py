4fa9189b68bf9badfff2641b6678501729acf9c0
MnrGDKL <munirgedikli.2021@gmail.com>
2022-05-26 22:44:19 +0300

    11

--- 4. Django/class-notes/011_DjangoAuthBuiltInStarterTemplate-mnrgdkl/user_example/views.py
+++ 4. Django/class-notes/011_DjangoAuthBuiltInStarterTemplate-mnrgdkl/user_example/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
 
 def home_view(request):    
     return render(request, "user_example/home.html")
@@ -6,3 +10,25 @@ def home_view(request):
 
 def special(request):    
     return render(request, "user_example/special.html")
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "registration/register.html", context)
