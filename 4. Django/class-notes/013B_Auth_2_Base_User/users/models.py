c2f0461547244ef45dbaf8d1b5498933c1dd063c
MnrGDKL <munirgedikli.2021@gmail.com>
2022-05-28 14:41:28 +0300

    11

--- 4. Django/class-notes/013B_Auth_2_Base_User/users/models.py
+++ 4. Django/class-notes/013B_Auth_2_Base_User/users/models.py
@@ -5,3 +5,16 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
   portfolio = models.URLField(blank=True)
   profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+  
+  #   username = models.CharField(
+  #     _('username'),
+  #     max_length=150,
+  #     unique=True,
+  #     help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+  #     validators=[username_validator],
+  #     error_messages={
+  #         'unique': _("A user with that username already exists."),
+  #     },
+  # )
+  username = models.EmailField("Email Address", unique=True)
+  REQUIRED_FIELDS = []
