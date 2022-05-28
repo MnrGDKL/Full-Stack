0e3f4870865c65231e5a824b8ecf0c06f2ced45a
MnrGDKL <munirgedikli.2021@gmail.com>
2022-05-28 13:42:13 +0300

    123

---
+++ 4. Django/class-notes/013B_Auth_2_Base_User/users/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+  portfolio = models.URLField(blank=True)
+  profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
