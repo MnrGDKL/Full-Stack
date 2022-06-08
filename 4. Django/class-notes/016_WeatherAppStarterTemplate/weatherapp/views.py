1a23f60f54a3a72b78a86ca5a814baa4cb69ab84
MnrGDKL <munirgedikli.2021@gmail.com>
2022-06-08 21:25:46 +0300

    11

--- 4. Django/class-notes/016_WeatherAppStarterTemplate/weatherapp/views.py
+++ 4. Django/class-notes/016_WeatherAppStarterTemplate/weatherapp/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
 from decouple import config
+from pprint import pprint
 
 def home(request):
     api_key = config('API_KEY')
-    city = "London"
+    city = "Kars"
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}'
     
     import requests
-    response = requests.get(url)
-    weather = response.json()
-    
-
-    return render(request, 'weatherapp/home.html')
+    r = requests.get(url)
+    if r.ok:
+        weather_data = r.json()
+        pprint(weather_data)
+        weather = {
+            'city' : weather_data['name'],
+            'temperature' : weather_data['main']['temp'],
+            'description' : weather_data['weather'][0]['description'],
+            'icon' : weather_data['weather'][0]['icon'],
+        }
+        context = {
+            'weather' : weather,
+        }
+        return render(request, 'weatherapp/home.html', context)
+    else:
+        print('Request failed')
+        return render(request, 'weatherapp/home.html')
