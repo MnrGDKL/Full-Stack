cd31bdbe5f581ccc0b8503da4acf4663fa4d5829
MnrGDKL <munirgedikli.2021@gmail.com>
2022-06-11 11:01:58 +0300

    11

--- 2. Javascript/4. Playground/charactercount/script.js
+++ 2. Javascript/4. Playground/charactercount/script.js
@@ -1,34 +1,6 @@
-let textarea = document.querySelector("textarea");
-let current = document.getElementById("current");
-console.log(current);
-let regexp = /[ğüşıİöç]/gi;
-let ikazz = document.getElementById("alper");
-
-function sayac() {
-  let yazi = textarea.value;
-  let boslukalinmis = yazi.replace(/ +/g, " ");
-  let turkcetekrarsayisi = (yazi.match(regexp) || []).length;
-  let uzunluk = boslukalinmis.length;
-  window.toplam = uzunluk + turkcetekrarsayisi;
-
-  current.innerText = toplam;
-
-  if (toplam <= 155) {
-    ikazz.classList.add("ikaz");
-    //textarea.maxLength = 156;
-  } else {
-    ikazz.classList.remove("ikaz");
-  }
+const apples = []
+for (let i = 0; i < 5; i++) {
+ apples.push(i)
+  
 }
-
-function gonder() {
-  let mesaj_sayi = Math.ceil(toplam / 155);
-
-  Swal.fire({
-    title: "Uyarı!",
-    html: "Toplam " + "<b>" + mesaj_sayi + " MESAJ" + "</b>" + " gönderilecek?",
-    icon: "warning",
-    confirmButtonText: "Tamam",
-  }).then(() => window.location.reload());
-}
-
+console.log(i)
