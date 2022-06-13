f78305adcad5455d8c055618f5db3607c57d5334
MnrGDKL <munirgedikli.2021@gmail.com>
2022-06-13 22:01:23 +0300

    123

---
+++ 4. Django/class-notes/021_REST API/dj-rest-fbv - mnr/apiTodo/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Todo
+
+# Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+  list_display = ('task', 'description', 'priority', 'done', 'createdDate', 'updatedDate')
+
+admin.site.register(Todo, TodoAdmin)
